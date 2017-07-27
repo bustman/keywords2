29f135557071a972f4c5e59a5ee9f92fdb09929d
bustman <to_man@mail.com>
2017-07-27 17:50:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import  time
+
+def benchmark(func):
+    def wrapper(*args, **kwargs):
+        t = time.clock()
+        res = func(*args, **kwargs)
+        log_pars.debug("%s time %.2f" %(func.__name__,time.clock() - t))
+        return res
+    return wrapper
+
